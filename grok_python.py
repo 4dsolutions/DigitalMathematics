d7703d13ce94699f16b92a93aa839fc656615f18
Kirby Urner <kirby.urner@gmail.com>
2024-02-02 15:10:53 -0800

    2nd article

--- grok_python.py
+++ grok_python.py
@@ -2,6 +2,10 @@
 Grokking Python:
 https://kirbyurner.medium.com/grokking-python-9f96140c1e07?sk=d1ec6762e753d5c79106faf1669fcb04
 (friend link)
+
+Python's Rib Cage:
+https://kirbyurner.medium.com/pythons-rib-cage-e4ff16cf3a74?sk=3e59d2356de6a13b42c9cf61d4d6a308
+(friend link)
 """
 
 class Dog:
@@ -14,6 +18,10 @@ class Dog:
         self.name = nm
         self.stomach = [ ] # born with an empty stomach
 
+    def __add__(self: Dog, other: Dog):  # type annotations!
+        "Combine names for the new doggie"
+        return Dog(self.name + other.name)
+        
     def eat(self, food):
         "Add a food to my stomach"
         self.stomach.append(food)
