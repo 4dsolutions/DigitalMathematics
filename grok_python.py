698542fd5645d5b2c12f7e84bd51973a22bdd307
Kirby Urner <kirby.urner@gmail.com>
2024-03-04 06:10:55 -0800

    Animal class

--- grok_python.py
+++ grok_python.py
@@ -6,21 +6,16 @@ https://kirbyurner.medium.com/grokking-python-9f96140c1e07?sk=d1ec6762e753d5c791
 Python's Rib Cage:
 https://kirbyurner.medium.com/pythons-rib-cage-e4ff16cf3a74?sk=3e59d2356de6a13b42c9cf61d4d6a308
 (friend link)
+
+In this more advanced version, we study inheritance
 """
 
-class Dog:
-    """
-    the Dog type
-    """
+class Animal:
 
     def __init__(self, nm):
-        "A dog is born..."
+        "An animal is born..."
         self.name = nm
         self.stomach = [ ] # born with an empty stomach
-
-    def __add__(self: Dog, other: Dog):  # type annotations!
-        "Combine names for the new doggie"
-        return Dog(self.name + other.name)
         
     def eat(self, food):
         "Add a food to my stomach"
@@ -28,4 +23,33 @@ class Dog:
 
     def __repr__(self):
         "Repper -- represent me"
-        return f"Dog named {self.name}"
+        return f"a {type(self).__name__} named {self.name}"
+    
+    def __add__(self, other):  
+        "Combine names for the new animal of self type"
+        return type(self)(self.name + other.name)
+
+class Cat(Animal):
+    """
+    the Cat type
+    """  
+    def meow(self, n):
+        return "Meow! " * n
+    
+class Dog(Animal):
+    """
+    the Dog type
+    """
+    def bark(self, n):
+        return "Meow! " * n
+
+
+if __name__ == "__main__":
+    rover = Dog("Rover")
+    kitty = Cat("Kitty")
+    print(rover)
+    print(kitty)
+    rover.eat(kitty)
+    print(rover.stomach)
+    rover.eat(rover)
+    print(rover.stomach)
